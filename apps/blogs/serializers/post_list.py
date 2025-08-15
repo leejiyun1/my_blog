700bdbe235f21e8481175a2c8e9c6e5130495b24
“leejiyun1” <poi20701556@gmail.com>
2025-08-15 20:17:50 +0900

    post_list

---
+++ apps/blogs/serializers/post_list.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models import Post
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'created_at', 'updated_at']
