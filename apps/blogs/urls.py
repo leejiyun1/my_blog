f01b5d364f62e93970e80b4f8c292386f5190dc1
“leejiyun1” <poi20701556@gmail.com>
2025-08-15 22:14:13 +0900

    미완성 댓글기능

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views.comment_create import CommentCreate
 from .views.post_create import PostCreate
 from .views.post_list import PostList
 from .views.post_detail import PostDetail
@@ -10,4 +11,7 @@ urlpatterns = [
     path('posts/', PostList, name='post_list'),
     path('posts/<int:pk>/', PostDetail, name='post_detail'),
     path('posts/create/', PostCreate, name='post_create'),
+    # path('posts/<int:pk>/comments/', CommentList, name='comment_list'),
+    path('comments/', CommentCreate, name='comment_create'),
+
 ]
